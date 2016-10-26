6f5e493113980126a97cf8eded17dc89a3f86471
Anh Duc Tran <anhduc.t92@gmail.com>
2016-10-26 11:23:01 +0200

    delete tabhost in mainactivity

--- app/src/main/java/com/example/duc/todoapp/MainActivity.java
+++ app/src/main/java/com/example/duc/todoapp/MainActivity.java
@@ -1,37 +1,18 @@
 package com.example.duc.todoapp;
 
-import android.app.TabActivity;
-import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TabHost;
 
-import com.github.mikephil.charting.charts.*;
 
-public class MainActivity extends TabActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Fragment fg = new Fragment();
-        fg.getFragmentManager().beginTransaction().add(R.id.toDoListView, fg).commit();
 
-        //TabHost host = (TabHost) findViewById(R.id.tabhost);
-        TabHost host = getTabHost();
-
-        TabHost.TabSpec spec = host.newTabSpec("Tab1");
-        spec.setContent(new Intent(this, ToDoListActivity.class));
-        spec.setIndicator("To-Do's");
-        host.addTab(spec);
-
-        spec = host.newTabSpec("Tab2");
-        spec.setContent(new Intent(this, StatisticsActivity.class));
-        spec.setIndicator("Statistics");
-        host.addTab(spec);
     }
 
 
